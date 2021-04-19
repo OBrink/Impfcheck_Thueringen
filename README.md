8a041545b83fc613a7a9beb8b008c9f065d3bd34
OBrink <brinkhaus@gmail.com>
2021-04-19 21:21:56 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Impfcheck_Thueringen
-This repository contains a script that checks regularily if the Thuringian vaccination information site has changed since the last check.
+This repository contains a very simple script that checks every 10 minutes 
+if the Thuringian vaccination information site has changed since the last check.
+If any changes are detected, it will make create a pop-up message to notify you.
+
+### Get started
+- Clone this repository.
+
+
+    git clone https://github.com/OBrink/Impfcheck_Thueringen
+
+- You might need to install beautifulsoup:
+
+    
+    pip install beautifulsoup4
+
+
+Simply run 
+
+
+    python  impf_checker.py
+
+The script will then run for 10 hours and check every 10 minutes if 
+something has changed since the last check. The text from the page 
+is saved in impfen_thueringen.txt in same directory.
---
+++ impf_checker.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import urllib.request
+import tkinter as tk
+from tkinter import ttk
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+def get_impfen_thueringen_content() -> bool:
+	'''This function gets checks the vaccination site where it says for which groups  registration 
+	is open right now and returns the content as a string. <script> parts are deleted as they change
+	whenever the page is loaded.'''
+	# Get HTML content of url
+	url = 'https://www.impfen-thueringen.de/terminvergabe.html'
+	req = urllib.request.urlopen(url)
+	html_string = req.read()
+	# Get text
+	soup = BeautifulSoup(html_string, features = 'lxml')
+	site_text = str(soup.get_text())
+	return site_text.encode('utf8')
+
+
+def popupmsg(message: str):
+	'''This function creates a popup window that shows a given message'''
+	popup = tk.Tk()
+	popup.wm_title("!")
+	label = ttk.Label(popup, text=message)
+	label.pack(side="top", fill="x", pady=10)
+	button = ttk.Button(popup, text="OK", command = popup.destroy)
+	button.pack()
+	popup.mainloop()
+	return
+
+
+def check_impfen_thueringen() -> bool:
+	'''This function checks whether or not the html has changed since the last check. If that is the case,
+	a notification will pop up and the updated version will be saved for the next check. It returns true
+	if the site has changed.'''
+	first_time = True
+	if 'impfen_thueringen.txt' in os.listdir('.'):
+		first_time = False
+		with open("impfen_thueringen.txt", 'r') as saved_file:
+			reference_html_content = saved_file.read()
+		up_to_date_html_content = get_impfen_thueringen_content()
+		if str(up_to_date_html_content) != str(reference_html_content):
+			popupmsg('The site has changed since the last check!')
+		else:
+			print('Nothing has changed.')
+	with open("impfen_thueringen.txt", 'w+') as saved_file:
+		saved_file.write(str(get_impfen_thueringen_content()))
+	# Return true if a change has been detected.
+	if not first_time:
+		if str(up_to_date_html_content) != str(reference_html_content):
+			return True
+
+
+def main():
+	'''This script runs for 10 hours and checks every 10 minutes if https://www.impfen-thueringen.de/terminvergabe.html has
+	changed since the last check. The html string is saved in the file impfen_thueringen.html (in the same folder as the script.)'''
+	for _ in range(60):
+		check = check_impfen_thueringen()
+		timestamp = datetime.now(tz=None)
+		if check:
+			print('Last checked at ' + str(timestamp) + '. Result: Something has changed! Go check if you can register.')
+		else:
+			print('Last checked at ' + str(timestamp) + '. Result: Nothing has changed!')
+		time.sleep(600)
+	sys.exit(1)
+
+
+if __name__ == '__main__':
+	main()
+
